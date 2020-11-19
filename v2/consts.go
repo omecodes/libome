750de290648e5a3331145af5eada90acd0be8db5
omecodes <omecodes@gmail.com>
2020-11-19 20:14:55 +0100

    Rollback to v1 content

--- v2/consts.go
+++
@@ -1,9 +0,0 @@
-package ome
-
-const (
-	MetaServiceCertificate = "service-certificate"
-	MetaTokenVerifyingKey  = "token-verify-key"
-
-	MetaApiAccessKey    = "api-access-credentials"
-	MetaApiAccessSecret = "api-access-secret"
-)
