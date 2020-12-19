a637bef3a9bdc8d4093eac39d06552ffa713d7c0
omecodes <omecodes@gmail.com>
2020-12-19 13:47:35 +0100

    Replace service type enum with uint32

--- consts.go
+++ consts.go
@@ -7,3 +7,16 @@ const (
 	MetaApiAccessKey    = "api-access-credentials"
 	MetaApiAccessSecret = "api-access-secret"
 )
+
+const (
+	UnknownServiceType        = 0
+	CAServiceType             = 1
+	AuthenticationServiceType = 2
+	OrganizationServiceType   = 3
+	TokenStoreServiceType     = 4
+	DataServiceType           = 5
+	FileStorageServiceType    = 6
+	AppRegistryServiceType    = 7
+	UserAccountsServiceType   = 8
+	FilesServerServiceType    = 9
+)
